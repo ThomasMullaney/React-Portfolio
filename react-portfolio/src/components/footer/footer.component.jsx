27eab694a25031ef16ee7f776248ae6562b21a76
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-12-01 08:05:12 -0800

    completed footer component and added images into assets

---
+++ react-portfolio/src/components/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./footer.style.css";
+
+const FooterPanel = () => {
+return (
+    <div>
+        <div className="py-2 text-center footer-style">
+            Created by Tom Mullaney&nbsp; {new Date().getFullYear()}
+        </div>
+    </div>
+);
+};
+
+
+export default FooterPanel;
