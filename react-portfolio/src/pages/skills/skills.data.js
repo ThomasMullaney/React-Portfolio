055f83f8e84ab0dc4af72bbca887dfed18f79476
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-12-02 09:41:30 -0800

    added skills page

---
+++ react-portfolio/src/pages/skills/skills.data.js
@@ -0,0 +1,35 @@
+// Frontend skills
+import L_Html5 from "../../assets/images/skills/html-5.svg"
+import L_Css3 from "../../assets/images/skills/css3.svg"
+import L_Bootstrap from "../../assets/images/skills/bootstrap-4.svg"
+import L_React from "../../assets/images/skills/react.svg"
+import L_React_Router from "../../assets/images/skills/react-router.svg"
+import L_React_Bootstrap from "../../assets/images/skills/react-bootstrap.svg"
+import L_MaterialUi from "../../assets/images/skills/material-ui-1.svg"
+import L_Electron from "../../assets/images/skills/electron.svg"
+
+// backend skills
+import L_Nodejs from "../../assets/images/skills/nodejs.svg"
+import L_Express from "../../assets/images/skills/express.svg"
+import L_Django from "../../assets/images/skills/django.svg"
+import L_Auth0 from "../../assets/images/skills/auth0.svg"
+
+// host
+import L_Heroku from "../../assets/images/skills/heroku.svg"
+import L_Github_Pages from "../../assets/images/skills/github.svg"
+import L_AWS from "../../assets/images/skills/aws.svg"
+
+//programming
+import L_Javascript from "../../assets/images/skills/javascript.svg"
+import L_Typescript from "../../assets/images/skills/typescript.svg"
+import L_Python from "../../assets/images/skills/python.svg"
+
+// database
+import L_MsSql from "../../assets/images/skills/mssql.svg"
+import L_Mysql from "../../assets/images/skills/mysql.svg"
+import L_Sqlite from "../../assets/images/skills/sqlite.svg"
+import L_Postgresql from "../../assets/images/skills/postgresql.svg"
+import L_MongoDB from "../../assets/images/skills/mongoDB.svg"
+
+// version 
+import L_Git from "../../assets/images/skills/git-icon.svg"
