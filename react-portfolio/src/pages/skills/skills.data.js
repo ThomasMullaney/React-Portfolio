42e9e8877bbaf82cb9952e0a2fdf67840218c37c
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-12-02 10:24:29 -0800

    built skills page

--- react-portfolio/src/pages/skills/skills.data.js
+++ react-portfolio/src/pages/skills/skills.data.js
@@ -1,35 +1,196 @@
 // Frontend skills
-import L_Html5 from "../../assets/images/skills/html-5.svg"
-import L_Css3 from "../../assets/images/skills/css3.svg"
-import L_Bootstrap from "../../assets/images/skills/bootstrap-4.svg"
-import L_React from "../../assets/images/skills/react.svg"
-import L_React_Router from "../../assets/images/skills/react-router.svg"
-import L_React_Bootstrap from "../../assets/images/skills/react-bootstrap.svg"
-import L_MaterialUi from "../../assets/images/skills/material-ui-1.svg"
-import L_Electron from "../../assets/images/skills/electron.svg"
+import L_Html5 from "../../assets/images/skills/html-5.svg";
+import L_Css3 from "../../assets/images/skills/css3.svg";
+import L_Bootstrap from "../../assets/images/skills/bootstrap-4.svg";
+import L_React from "../../assets/images/skills/react.svg";
+import L_React_Router from "../../assets/images/skills/react-router.svg";
+import L_React_Bootstrap from "../../assets/images/skills/react-bootstrap.svg";
+import L_MaterialUi from "../../assets/images/skills/material-ui-1.svg";
+import L_Electron from "../../assets/images/skills/electron.svg";
 
 // backend skills
-import L_Nodejs from "../../assets/images/skills/nodejs.svg"
-import L_Express from "../../assets/images/skills/express.svg"
-import L_Django from "../../assets/images/skills/django.svg"
-import L_Auth0 from "../../assets/images/skills/auth0.svg"
+import L_Nodejs from "../../assets/images/skills/nodejs.svg";
+import L_Express from "../../assets/images/skills/express.svg";
+import L_Django from "../../assets/images/skills/django.svg";
+import L_Auth0 from "../../assets/images/skills/auth0.svg";
 
 // host
-import L_Heroku from "../../assets/images/skills/heroku.svg"
-import L_Github_Pages from "../../assets/images/skills/github.svg"
-import L_AWS from "../../assets/images/skills/aws.svg"
+import L_Heroku from "../../assets/images/skills/heroku.svg";
+import L_Github_Pages from "../../assets/images/skills/github.svg";
+import L_AWS from "../../assets/images/skills/aws.svg";
 
 //programming
-import L_Javascript from "../../assets/images/skills/javascript.svg"
-import L_Typescript from "../../assets/images/skills/typescript.svg"
-import L_Python from "../../assets/images/skills/python.svg"
+import L_Javascript from "../../assets/images/skills/javascript.svg";
+import L_Typescript from "../../assets/images/skills/typescript.svg";
+import L_Python from "../../assets/images/skills/python.svg";
 
 // database
-import L_MsSql from "../../assets/images/skills/mssql.svg"
-import L_Mysql from "../../assets/images/skills/mysql.svg"
-import L_Sqlite from "../../assets/images/skills/sqlite.svg"
-import L_Postgresql from "../../assets/images/skills/postgresql.svg"
-import L_MongoDB from "../../assets/images/skills/mongoDB.svg"
+import L_MsSql from "../../assets/images/skills/mssql.svg";
+import L_Mysql from "../../assets/images/skills/mysql.svg";
+import L_Sqlite from "../../assets/images/skills/sqlite.svg";
+import L_Postgresql from "../../assets/images/skills/postgresql.svg";
+import L_MongoDB from "../../assets/images/skills/mongoDB.svg";
 
 // version 
-import L_Git from "../../assets/images/skills/git-icon.svg"
+import L_Git from "../../assets/images/skills/git-icon.svg";
+
+export const skills = {
+    frontend: [
+      {
+        link: "https://en.wikipedia.org/wiki/HTML5",
+        imgAltText: "HTML 5",
+        imgSrc: L_Html5,
+        skillName: "HTML5",
+      },
+      {
+        link:
+          "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
+        imgAltText: "CSS 3",
+        imgSrc: L_Css3,
+        skillName: "CSS3",
+      },
+      {
+        link: "https://getbootstrap.com/",
+        imgAltText: "Bootstrap",
+        imgSrc: L_Bootstrap,
+        skillName: "Bootstrap",
+      },
+      {
+        link: "https://reactjs.org/",
+        imgAltText: "React JS",
+        imgSrc: L_React,
+        skillName: "React JS",
+      },
+      {
+        link: "https://reacttraining.com/react-router/",
+        imgAltText: "React Router",
+        imgSrc: L_React_Router,
+        skillName: "React Router",
+      },
+      {
+        link: "https://react-bootstrap.github.io/",
+        imgAltText: "React Bootstrap",
+        imgSrc: L_React_Bootstrap,
+        skillName: "React Bootstrap",
+      },
+      {
+        link: "https://material-ui.com/",
+        imgAltText: "Material-UI",
+        imgSrc: L_MaterialUi,
+        skillName: "Material-UI",
+      },
+      {
+        link: "https://electronjs.org/",
+        imgAltText: "Electron",
+        imgSrc: L_Electron,
+        skillName: "Electron",
+      },
+    ],
+  
+    backend: [
+      {
+        link: "https://nodejs.org/en/",
+        imgAltText: "Node.js",
+        imgSrc: L_Nodejs,
+        skillName: "Node.js",
+      },
+      {
+        link: "https://expressjs.com/",
+        imgAltText: "Express",
+        imgSrc: L_Express,
+        skillName: "Express",
+      },
+      {
+        link: "https://auth0.com/",
+        imgAltText: "Auth0",
+        imgSrc: L_Auth0,
+        skillName: "Auth0",
+      },
+      {
+        link: "https://auth0.com/",
+        imgAltText: "Django",
+        imgSrc: L_Django,
+        skillName: "Django",
+      },
+    ],
+    hostingPlatforms: [
+      {
+        link: "https://www.heroku.com/",
+        imgAltText: "Heroku",
+        imgSrc: L_Heroku,
+        skillName: "Heroku",
+      },
+      {
+        link: "https://pages.github.com/",
+        imgAltText: "GitHub Pages",
+        imgSrc: L_Github_Pages,
+        skillName: "GitHub Pages",
+      },
+      {
+        link: "https://www.aws.amazoncom/",
+        imgAltText: "Amazon Web Services",
+        imgSrc: L_AWS,
+        skillName: "Amazon Web Services",
+      },
+    ],
+    programmingLanguages: [
+      {
+        link: "https://www.javascript.com/",
+        imgAltText: "JavaScript",
+        imgSrc: L_Javascript,
+        skillName: "JavaScript",
+      },
+      {
+        link: "https://www.typescriptlang.org/",
+        imgAltText: "TypeScript",
+        imgSrc: L_Typescript,
+        skillName: "TypeScript",
+      },
+      {
+        link: "https://www.python.org/",
+        imgAltText: "Python",
+        imgSrc: L_Python,
+        skillName: "Python",
+      },
+    ],
+    databases: [
+      {
+        link: "https://www.microsoft.com/en-us/sql-server/sql-server-2019",
+        imgAltText: "MS-SQL",
+        imgSrc: L_MsSql,
+        skillName: "MS-SQL",
+      },
+      {
+        link: "https://www.mysql.com/",
+        imgAltText: "My-SQL",
+        imgSrc: L_Mysql,
+        skillName: "My-SQL",
+      },
+      {
+        link: "https://www.sqlite.org/",
+        imgAltText: "SQLite",
+        imgSrc: L_Sqlite,
+        skillName: "SQLite",
+      },
+      {
+        link: "https://www.postgresql.org/",
+        imgAltText: "PostgreSQL",
+        imgSrc: L_Postgresql,
+        skillName: "PostgreSQL",
+      },
+      {
+        link: "https://www.mongodb.com/",
+        imgAltText: "MongoDB",
+        imgSrc: L_MongoDB,
+        skillName: "MongoDB",
+      },
+    ],
+    versionControl: [
+      {
+        link: "https://git-scm.com/",
+        imgAltText: "GIT",
+        imgSrc: L_Git,
+        skillName: "GIT",
+      },
+    ],
+  };
