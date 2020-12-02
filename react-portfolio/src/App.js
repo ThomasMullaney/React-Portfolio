6ee8757fb1a09b7face9f43078062d4bb4a14d26
Thomas Mullaney <tloring.mullaney@gmail.com>
2020-12-02 10:47:29 -0800

    app.js built within src folder

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Fade from "react-reveal/Fade";
+import { Parallax } from "react-bootstrap/Container";
 
-function App() {
+// components
+import MyNavbar from "./components/navbar/navbar.component";
+import MyFooter from "./components/footer/footer.component";
+import MyCarousel from "./components/carousel/carousel.component";
+import TitleMessage from "./components/title-message/message.component";
+import TimeLine from "./components/timeline/timeline.component";
+import Slide from "react-reveal/Slide";
+import About from "./pages/about/about.component";
+import Skills from "./pages/skills/skills.component";
+import Contact from "./pages/contact/contact.component";
+
+import "./App.css";
+import FooterPanel from "./components/footer/footer.component";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ position: "relative" }}>
+      <MyCarousel />
+      <TitleMessage />
+      <MyNavbar />
+      {/* Particles
+          className="particles particles-box"
+          params={particlesOptions}
+      /> */}
+      <div>
+        <Parallax
+          blur={{ min: -30, max: 30 }}
+          bgImage={require("./assets/img/parallex/background.webp")}
+          bgImageAlt=""
+          strength={-200}
+          >
+            <div>
+              <Container className="container-box rounded">
+                <Fade duration={500}>
+                  <About />
+                </Fade>
+              </Container>
+            </div>
+          </Parallax>
+      </div>
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
+      <div>
+      <Container className="container-box rounded">
+                <Fade duration={500}>
+                  <hr />
+                </Fade>
+              </Container>
+      </div>
+      <Container className="container-box rounded">
+                <Slide bottom duration={500}>
+                  <hr />
+                  <TimeLine />
+                </Slide>
+      </Container>
+      <Container className="container-box rounded">
+                <Fade duration={500}>
+                  <hr />
+                  <ContactForm />
+                </Fade>
+              </Container>
+      
+      <hr />
+      <FooterPanel />
     </div>
   );
-}
+};
 
 export default App;
